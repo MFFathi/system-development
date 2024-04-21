3d2cd95b7e13073d59f464ca82b5c3613dd54a33
Faded6 <125335333+Faded6@users.noreply.github.com>
2024-04-21 17:27:43 +0100

    eventmanagement.py

--- src/eventmanagement.py
+++ src/eventmanagement.py
@@ -8,7 +8,7 @@ class EventManagementPage(tk.Tk):
         super().__init__()
 
         self.title("Event Management")
-        self.geometry("600x400")
+        self.geometry("600x600")  
 
         self.connection = mysql.connector.connect(
             host="127.0.0.1",
@@ -28,74 +28,124 @@ class EventManagementPage(tk.Tk):
         style.theme_use("clam")
         style.configure("Treeview.Heading", background="black", foreground="white")
 
-        # Entry section for Event Name
+        # This is the entry section for Event Name- 22066867
         label_name = tk.Label(eventmanagement_frame, text="Event Name", font=('Consolas', 10, 'bold'), fg="black", background="white")
         label_name.pack(anchor=tk.N, pady=5)
         entry_name = tk.Entry(eventmanagement_frame, bd=0, width=30)
         entry_name.pack(anchor=tk.N, pady=5)
 
-        # Entry section for Event Description
+        # This is the entry section for Event Description- 22066867
         label_description = tk.Label(eventmanagement_frame, text="Event Description", font=('Consolas', 10, 'bold'), fg="black", background="white")
         label_description.pack(anchor=tk.N, pady=5)
         entry_description = tk.Entry(eventmanagement_frame, bd=0, width=30)
         entry_description.pack(anchor=tk.N, pady=5)
 
-        # Entry section for Date of Event
+        # This is the entry section for Date of Event- 22066867
         label_date = tk.Label(eventmanagement_frame, text="Date of Event (YYYY-MM-DD)", font=('Consolas', 10, 'bold'), fg="black", background="white")
         label_date.pack(anchor=tk.N, pady=5)
         entry_date = tk.Entry(eventmanagement_frame, bd=0, width=30)
         entry_date.pack(anchor=tk.N, pady=5)
 
-        # Label for Event Status
+        # This is the entry section for the Start Time- 22066867
+        label_start_time = tk.Label(eventmanagement_frame, text="Start Time (HH:MM)", font=('Consolas', 10, 'bold'), fg="black", background="white")
+        label_start_time.pack(anchor=tk.N, pady=5)
+        entry_start_time = tk.Entry(eventmanagement_frame, bd=0, width=30)
+        entry_start_time.pack(anchor=tk.N, pady=5)
+
+        # This is the entry section for End Time- 22066867
+        label_end_time = tk.Label(eventmanagement_frame, text="End Time (HH:MM)", font=('Consolas', 10, 'bold'), fg="black", background="white")
+        label_end_time.pack(anchor=tk.N, pady=5)
+        entry_end_time = tk.Entry(eventmanagement_frame, bd=0, width=30)
+        entry_end_time.pack(anchor=tk.N, pady=5)
+
+        label_type = tk.Label(eventmanagement_frame, text="Event Type", font=('Consolas', 10, 'bold'), fg="black", background="white")
+        label_type.pack(anchor=tk.N, pady=5)
+
+        type_options = ["Birthday", "Anniversary", "Work Party"]
+        combo_type = ttk.Combobox(eventmanagement_frame, values=type_options)
+        combo_type.pack(anchor=tk.N, pady=5)
+
+        # This is the entry section for the Phone Number- 22066867
+        label_phone = tk.Label(eventmanagement_frame, text="Phone Number", font=('Consolas', 10, 'bold'), fg="black", background="white")
+        label_phone.pack(anchor=tk.N, pady=5)
+        entry_phone = tk.Entry(eventmanagement_frame, bd=0, width=30)
+        entry_phone.pack(anchor=tk.N, pady=5)
+
+        # This is the entry section for the email- 22066867
+        label_email = tk.Label(eventmanagement_frame, text="Email", font=('Consolas', 10, 'bold'), fg="black", background="white")
+        label_email.pack(anchor=tk.N, pady=5)
+        entry_email = tk.Entry(eventmanagement_frame, bd=0, width=30)
+        entry_email.pack(anchor=tk.N, pady=5)
+
+        # This is the entry section for Address- 22066867
+        label_address = tk.Label(eventmanagement_frame, text="Address", font=('Consolas', 10, 'bold'), fg="black", background="white")
+        label_address.pack(anchor=tk.N, pady=5)
+        entry_address = tk.Entry(eventmanagement_frame, bd=0, width=30)
+        entry_address.pack(anchor=tk.N, pady=5)
+
         label_status = tk.Label(eventmanagement_frame, text="Event Status", font=('Consolas', 10, 'bold'), fg="black", background="white")
         label_status.pack(anchor=tk.N, pady=5)
 
-        # Combobox for Event Status
+        # This is the combobox for Event Status- 22066867
         status_options = ["Accept", "No Action taken yet", "Finished", "Cancelled", "Reject"]
         combo_status = ttk.Combobox(eventmanagement_frame, values=status_options)
         combo_status.pack(anchor=tk.N, pady=5)
 
-        # Button to submit
-        submit_button = tk.Button(eventmanagement_frame, text="Submit", command=lambda: self.submit_button_clicked(entry_name, entry_description, entry_date, combo_status), background="#FF0000", fg="white", font=('Consolas', 10, 'bold'), width=6, height=2, bd=0, cursor="hand2")
+        # This is the button to submit- 22066867
+        submit_button = tk.Button(eventmanagement_frame, text="Submit", command=lambda: self.submit_button_clicked(entry_name, entry_description, entry_date, entry_start_time, entry_end_time, combo_type, entry_phone, entry_email, entry_address, combo_status), background="#FF0000", fg="white", font=('Consolas', 10, 'bold'), width=6, height=2, bd=0, cursor="hand2")
         submit_button.pack(ipadx=20, ipady=20, padx=10, pady=10)
 
-        # Button to view and modify the Event Status
+        # This is the button to view and modify the Event Status- 22066867
         view_modify_button = tk.Button(eventmanagement_frame, text="View and Modify Status", command=self.view_modify_status, background="#0000FF", fg="white", font=('Consolas', 10, 'bold'), width=25, height=2, bd=0, cursor="hand2")
         view_modify_button.pack(ipadx=20, ipady=20, padx=10, pady=10)
 
-        # Store Treeview values by EventID
+        # This is to store Treeview values by EventID- 22066867
         self.events_dict = {}
 
-    def submit_button_clicked(self, entry_name, entry_description, entry_date, combo_status):
+    def submit_button_clicked(self, entry_name, entry_description, entry_date, entry_start_time, entry_end_time, combo_type, entry_phone, entry_email, entry_address, combo_status):
         print("Debugging: Starting using the submit_button_clicked method")
 
-        # Retrieve values from entry fields
+        # This is to retrieve values from entry fields- 22066867
         name = entry_name.get()
         description = entry_description.get()
         date_str = entry_date.get()
+        start_time = entry_start_time.get()
+        end_time = entry_end_time.get()
+        event_type = combo_type.get()
+        phone = entry_phone.get()
+        email = entry_email.get()
+        address = entry_address.get()
         status = combo_status.get()
 
-        # Validate all fields are filled
-        if not name or not description or not date_str or not status:
+        # This is to validate if all the fields are filled- 22066867
+        if not name or not description or not date_str or not start_time or not end_time or not event_type or not phone or not email or not address or not status:
             messagebox.showerror("Error", "All fields must be filled.")
             return
 
-        # Validate the date format
+        # This is to validate the date format- 22066867
         try:
             date = datetime.strptime(date_str, "%Y-%m-%d").date()
         except ValueError:
             messagebox.showerror("Error", "Invalid date format, it must be in this format YYYY-MM-DD.")
             return
 
-        # Check if the date entered is after the current date
+        # This is to validate the time format- 22066867
+        try:
+            datetime.strptime(start_time, "%H:%M")
+            datetime.strptime(end_time, "%H:%M")
+        except ValueError:
+            messagebox.showerror("Error", "Invalid time format, it must be in this format HH:MM.")
+            return
+
+        # This is to check if the date entered is after the current date- 22066867
         current_date = datetime.now().date()
         if date <= current_date:
             messagebox.showerror("Error", "Date of Event must be after the current date.")
             return
 
-        # Insert the data into events table
-        query = "INSERT INTO events (EventName, EventDescription, DateOfEvent, EventStatus) VALUES (%s, %s, %s, %s)"
-        values = (name, description, date, status)
+        # This is to insert the data into events table- 22066867
+        query = "INSERT INTO events (EventName, EventDescription, DateOfEvent, StartTime, EndTime, EventType, PhoneNumber, Email, Address, EventStatus) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        values = (name, description, date, start_time, end_time, event_type, phone, email, address, status)
 
         try:
             self.cursor.execute(query, values)
@@ -107,13 +157,17 @@ class EventManagementPage(tk.Tk):
     def view_modify_status(self):
         view_events_window = tk.Toplevel(self)
         view_events_window.title("View and Modify Status of Events")
-        view_events_window.geometry("600x400")
-
-        events_treeview = ttk.Treeview(view_events_window, columns=("EventID", "EventName", "EventDescription", "DateOfEvent", "EventStatus"))
-        events_treeview.heading("EventID", text="Event ID")
+        view_events_window.geometry("800x400") 
+        events_treeview = ttk.Treeview(view_events_window, columns=("EventName", "EventDescription", "DateOfEvent", "StartTime", "EndTime", "EventType", "PhoneNumber", "Email", "Address", "EventStatus"))
         events_treeview.heading("EventName", text="Event Name")
         events_treeview.heading("EventDescription", text="Event Description")
         events_treeview.heading("DateOfEvent", text="Date of Event")
+        events_treeview.heading("StartTime", text="Start Time")
+        events_treeview.heading("EndTime", text="End Time")
+        events_treeview.heading("EventType", text="Event Type")
+        events_treeview.heading("PhoneNumber", text="Phone Number")
+        events_treeview.heading("Email", text="Email")
+        events_treeview.heading("Address", text="Address")
         events_treeview.heading("EventStatus", text="Event Status")
 
         events_treeview.pack(padx=10, pady=10)
@@ -121,16 +175,15 @@ class EventManagementPage(tk.Tk):
         modify_status_button = tk.Button(view_events_window, text="Modify Status", command=lambda: self.modify_event_status(events_treeview), background="#0000FF", fg="white", font=('Consolas', 10, 'bold'), width=15, height=2, bd=0, cursor="hand2")
         modify_status_button.pack(pady=10)
 
-        query = "SELECT EventID, EventName, EventDescription, DateOfEvent, EventStatus FROM events"
+        query = "SELECT EventName, EventDescription, DateOfEvent, StartTime, EndTime, EventType, PhoneNumber, Email, Address, EventStatus FROM events"
         self.cursor.execute(query)
         events = self.cursor.fetchall()
 
         for event in events:
-            event_id = event[0]
-            if not events_treeview.exists(event_id):
-                events_treeview.insert("", "end", iid=event_id, tags=(event_id,), values=event)
+            if not events_treeview.exists(event[0]):
+                events_treeview.insert("", "end", values=event)
 
-                self.events_dict[event_id] = event
+                self.events_dict[event[0]] = event
 
     def modify_event_status(self, events_treeview):
         selected_item = events_treeview.selection()
@@ -147,14 +200,14 @@ class EventManagementPage(tk.Tk):
 
         new_status = simpledialog.askstring("Edit Status", f"Current Status: {current_status}\nPlease enter the new status:")
 
-        # Validate the entered status
+        # To validate the entered status- 22066867
         allowed_statuses = ["Accept", "No Action taken yet", "Finished", "Cancelled", "Reject"]
 
         if new_status not in allowed_statuses:
             messagebox.showerror("Error", "Invalid status. Please enter one of the following: Accept, No Action taken yet, Finished, Cancelled, Reject.")
             return
 
-        # In case the user cancels
+        # In case the user cancels- 22066867
         if not new_status:
             return  
 
