73b646de92eda5bc457f487928cf6f9a51d6fa9e
fxl2-lubinda <159076100+fxl2-lubinda@users.noreply.github.com>
2024-04-22 10:39:38 +0100

    Update Discounts.py

--- src/Discounts/Discounts.py
+++ src/Discounts/Discounts.py
@@ -17,6 +17,7 @@ class Discounts():
         self.credentials = credentials
         self.branch = branch
         self.root = root
+        self.table = ttk.Treeview(self.root,columns=('description', 'multiplier'), show='headings')
         
         discounts_label = ttk.Label(root,text="Discounts",font=("Arial", 28))
 
@@ -60,7 +61,6 @@ class Discounts():
         
         try:
             multiplier = float(multiplier)
-            print(multiplier)
         except ValueError:
             messagebox.showerror("Error", "Multiplier must be a number")
             return
@@ -89,32 +89,34 @@ class Discounts():
         for x in widgets:
             x.pack_forget()
             
-        table = ttk.Treeview(self.root,columns=('description', 'multiplier'), show='headings')
-        table.heading('description', text = "Description")
-        table.heading('multiplier', text = "Multiplier")
+        for i in self.table.get_children():
+            self.table.delete(i)
+            
+        self.table.heading('description', text = "Description")
+        self.table.heading('multiplier', text = "Multiplier")
         
-        table.pack(fill='both',expand=TRUE)   
+        self.table.pack(fill='both',expand=TRUE)   
         
         if operation == "view":
             back_button = ttk.Button(self.root, text="Back",command=lambda:self.goBackToDiscounts(widgets),width=20)
             back_button.pack()
-            widgets = [table, back_button]
+            widgets = [self.table, back_button]
             
         elif operation == "update":
-            update_button = ttk.Button(self.root,text="Update",command=lambda:self.updateDiscounts(widgets,table))
+            update_button = ttk.Button(self.root,text="Update",command=lambda:self.updateDiscounts(widgets))
             back_button = ttk.Button(self.root, text="Back",command=lambda:self.goBackToDiscounts(widgets))
     
             update_button.pack()
             back_button.pack()
-            widgets = [table, update_button, back_button]
+            widgets = [self.table, update_button, back_button]
             
         elif operation == "delete":
-            delete_button = ttk.Button(self.root,text="Delete",command=lambda:self.deleteDiscounts(widgets,table))  
+            delete_button = ttk.Button(self.root,text="Delete",command=lambda:self.deleteDiscount(widgets))  
             back_button = ttk.Button(self.root, text="Back",command=lambda:self.goBackToDiscounts(widgets)) 
             
             delete_button.pack()
             back_button.pack()
-            widgets = [table, delete_button, back_button]  
+            widgets = [self.table, delete_button, back_button]  
                     
                      
         viewDiscount = discounts
@@ -123,31 +125,71 @@ class Discounts():
             description =  x[0]
             multipler = x[1][0]
             data = (description,multipler)
-            table.insert(parent = '', index=0, values= data)
+            self.table.insert(parent = '', index=0, values= data)
     
     
     def updateDiscounts(self,widgets):
-        pass
+        
+        def submit(description, multiplier):
+            submit = messagebox.askyesno("Submit Changes?","Are You Sure You Want To Submit These Changes")
+            if submit:
+                Discount.set_multiplier(originalDescription,multiplier)
+                Discount.set_description(originalDescription,description)
+                messagebox.showinfo("Discount Updated","Discount Successfully Updated")
+                self.viewDiscounts(widgets,"update")
+        
+        discountInfo = []
+        for i in self.table.selection():
+            a = tuple((self.table.item(i)['values']))
+            if a[0] != " ":
+                discountInfo.append(self.table.item(i)['values'])
+                
+                #Asks for confirmation for the selected user to be updated:
+                yes_no = messagebox.askyesno("Update This User?", "Are You Sure You Want To Make Changes To This User?")
+                if (yes_no) == False: 
+                    self.viewUsers(widgets,"update")
+                else:
+                    for x  in widgets:
+                        x.pack_forget()
+                    description_label = ttk.Label(self.root, text="Description:")
+                    description_entry = ttk.Entry(self.root, width=50)
+                    multiplier_label = ttk.Label(self.root, text="Multiplier:")
+                    multiplier_entry = ttk.Entry(self.root, width=50)
+
+                    submit_button = ttk.Button(self.root, text="Submit", command=lambda: submit(description_entry.get(),multiplier_entry.get()))
+                    cancel_button = ttk.Button(self.root, text="Cancel", command=lambda: self.back(widgets))
+
+                    description_label.pack()
+                    description_entry.pack()
+                    multiplier_label.pack()
+                    multiplier_entry.pack()
+                    submit_button.pack()
+                    cancel_button.pack()
+                    
+                    originalDescription = discountInfo[0][0]
+                    description_entry.insert(0, discountInfo[0][0])
+                    multiplier_entry.insert(0,discountInfo[0][1])
+                    
+                    widgets = [description_label, description_entry, multiplier_label, multiplier_entry, submit_button, cancel_button]
+                        
+                        
     
     def deleteDiscount(self, widgets):
-        
-        selected_discounts = self.retrieve_selected_discounts()
 
-        if not selected_discounts:
-            messagebox.showinfo("Information", "No discounts selected.")
-            return
-
-        Discountinfo = []
-        for discount in selected_discounts:
-            if discount.get('description') and discount.get('multiplier'):
-                Discountinfo.append((discount['description'], discount['multiplier']))
-                yes_no = messagebox.askyesno("Delete Discount?", f"Are you sure you want to delete: {discount['description']}")
-
-                if yes_no:
-                    self.perform_discount_deletion(discount)
-                    
-        messagebox.showinfo("Deletion Complete", "Discount(s) deleted successfully.")
-        self.viewDiscounts(widgets,"delete")
+        discountInfo = []
+        for i in self.table.selection():
+            a = tuple((self.table.item(i)['values']))
+            if a[0] != " ":
+                discountInfo.append(self.table.item(i)['values'])
+                
+                #Asks for confirmation for the selected user to be updated:
+                yes_no = messagebox.askyesno("Delete This Discount?", "Are You Sure You Want To Delete This Discount?")
+                if (yes_no): 
+                    description = discountInfo[0][0]
+                    multiplier = discountInfo[0][1]
+                    Discount.delete(description,multiplier)
+                    messagebox.showinfo("Deletion Complete", "Discount(s) deleted successfully.")
+                    self.viewDiscounts(widgets,"delete")
     
     def goBackToDiscounts(self, widgets):
         for x in widgets:
